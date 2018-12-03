f8365e3a3fc1644381d25769142918f81034e1c8
Natan Facchin <n.facchin@sig.eu>
2018-12-03 15:46:35 +0100

    Refactor to use classes

---
+++ api/Message.php
@@ -0,0 +1,55 @@
+<?php
+require 'connect.php';
+require 'User.php';
+
+final class Message
+{
+    public static function sendMessage($from_username, $to_username, $text) {
+        $to_uid = User.getUserIdByUserName($to_username);
+
+        if ($to_uid == false) {
+            throw new Exception('Recipient user does NOT exist.');
+            exit;
+        }
+
+        $from_userid = User.getUserIdByUserName($from_username);
+
+        if ($from_userid == false) {
+            throw new Exception('Sender user does NOT exist.');
+            exit;
+        }
+
+        // Add message to message table
+        $db->exec('BEGIN');
+        $result = $db->query("INSERT INTO 'messages' (user_id, from_user, message)
+            VALUES ('$to_uid', '$from_userid', '$msg_text')");
+
+        $db->exec('COMMIT');
+
+        $db->close();
+
+        return $result;
+    }
+
+    public static function getMessagesFromUser($username) {
+        $uid = User.getUserIdByUserName($username);
+
+        if ($uid == false) {
+            throw new Exception('User does NOT exist.');
+        }
+
+        // Get by user_id
+
+        $pdo = getPDO();
+
+        $all_msgs = $pdo -> query("SELECT * FROM 'messages' WHERE user_id = '$uid' ORDER BY time DESC");
+
+        $all_results = $all_msgs->fetchAll();
+
+        $json = json_encode($all_results);
+
+        $db->close();
+
+        return $json;
+    }
+}
--- api/getMessages.php
+++ api/getMessages.php
@@ -3,35 +3,14 @@
  * Returns the list of messages.
  */
 
-require 'connect.php';
+require 'Message.php';
 
 // Sanitize.
 $username = $_GET['username'];
 
-$uid = getUserIdByUserName($username);
-
-if ($uid == false) {
-    http_response_code(404);
-    exit;
-}
-
-// Get by user_id
-
-$pdo = getPDO();
-
-$all_msgs = $pdo -> query("SELECT * FROM 'messages' WHERE user_id = '$uid' ORDER BY time DESC");
-
-$all_results = $all_msgs->fetchAll();
-
-$json = json_encode($all_results);
-
-echo $json;
-
-$db->close();
-
-function getUserIdByUserName($username) {
-    $database = getDB();
-    // Get by username.
-    $sql = "SELECT id FROM 'users' WHERE username = '$username' ";
-    return $database->querySingle($sql, false);
+try {
+    $result = Message.getMessagesFromUser($username);
+    echo $result;
+} catch (Exception $e) {
+    http_response_code(400);
 }
